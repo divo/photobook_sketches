2572f68bd7baf918f2a4075a93eb9b28867a1954
divo <divoapple@gmail.com>
2022-12-22 18:07:08 +0000

    Revert "Set drawing mode to glyph for manual refinement"
    This reverts commit 6fd721992f1af9ee0e5a6c35910667668e70fc0b.

--- draw_text.js
+++ draw_text.js
@@ -5,7 +5,6 @@ export const draw_text = (text, x, y, fontStyle, fontSize, context) => {
   //const y_scale = Math.ceil(transform.d);
   const y_scale = (transform.d);
   context.setTransform(1, 0, 0, 1, 0, 0); // There is a bug in Cairo related to rendering text in a transformed canvas
-  context.textDrawingMode = 'glyph'
   context.font = `${fontStyle} ${fontSize * x_scale}pt Helvetica`;
 
   context.fillText(text, x * x_scale, y * y_scale);
